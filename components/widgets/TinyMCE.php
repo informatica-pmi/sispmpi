c5af4f255d070a1e52e895dd9e413cfa5af7e5fb
STI - Prefeitura de Itabirito <informatica@pmi.mg.gov.br>
2026-05-05 17:44:42 -0300

    Alteração config TinyMCE

--- components/widgets/TinyMCE.php
+++ components/widgets/TinyMCE.php
@@ -2,23 +2,42 @@
 
 namespace app\components\widgets;
 
+/**
+ * Custom TinyMCE widget to standardise configurations across SisPMPI.
+ */
 class TinyMCE extends \dosamigos\tinymce\TinyMce
 {
+    /**
+     * @var string The TinyMCE API Key for Cloud usage.
+     */
+    public $apiKey;
+
     /**
      * @throws \yii\base\InvalidConfigException
      */
     public function init()
     {
+        // Define a linguagem padrão
         $this->language = 'pt_BR';
-        $this->options = ['rows' => 6];
-        $this->clientOptions = [
+        
+        // Configurações do campo de texto (HTML)
+        $this->options = array_merge(['rows' => 6], $this->options);
+        
+        // Configurações do editor (JavaScript)
+        $this->clientOptions = array_merge([
             'menubar' => false,
             'statusbar' => false,
             'toolbar' => "undo redo | bold italic | alignleft aligncenter alignright alignjustify",
             'entity_encoding' => 'raw',
             'paste_as_text' => true,
             'content_style' => 'body { font-family: Calibri; }'
-        ];
+        ], $this->clientOptions);
+
+        // Se a chave foi definida via DI (web.php), passamos para o componente pai
+        if ($this->apiKey) {
+            parent::$apiKey = $this->apiKey;
+        }
+
         parent::init();
     }
 }
