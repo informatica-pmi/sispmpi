032a6070026f5424562f80d2322b3478775d78bc
STI - Prefeitura de Itabirito <informatica@pmi.mg.gov.br>
2026-05-06 13:45:34 -0300

    Update TinyMCE.php

--- components/widgets/TinyMCE.php
+++ components/widgets/TinyMCE.php
@@ -5,33 +5,46 @@ namespace app\components\widgets;
 use Yii;
 use yii\helpers\ArrayHelper;
 
+/**
+ * Widget TinyMCE customizado para o SisPMPI.
+ * Força o carregamento via CDN para evitar erros de plugins locais ausentes.
+ */
 class TinyMCE extends \dosamigos\tinymce\TinyMce
 {
+    /**
+     * @var string Chave de API do TinyMCE
+     */
     public $apiKey;
 
+    /**
+     * @inheritdoc
+     */
     public function init()
     {
-        // 1. Tenta pegar a chave de todas as formas possíveis
+        // 1. Captura a chave de API
         $this->apiKey = $this->apiKey ?: getenv('TINYMCE_API_KEY') ?: (Yii::$app->params['tinyMceApiKey'] ?? null);
 
-        // 2. Se achamos a chave, forçamos ela no componente pai explicitamente
-        if ($this->apiKey) {
-            // Alguns wrappers usam a propriedade do objeto, outros esperam no config
-            $this->apiKey = $this->apiKey; 
+        // 2. Se tivermos uma chave, forçamos o carregamento via CDN oficial
+        // Isso impede que o sistema procure plugins na pasta /assets/ local
+        if ($this->apiKey && $this->apiKey !== 'no-api-key') {
+            $this->clientOptions['script_url'] = "https://cdn.tiny.cloud/1/{$this->apiKey}/tinymce/6/tinymce.min.js";
         }
 
-        $this->language = 'pt_BR';
-
+        // 3. Configurações de interface
         $defaultClientOptions = [
             'menubar' => false,
             'statusbar' => false,
-            'toolbar' => "undo redo | bold italic | alignleft aligncenter alignright alignjustify",
-            'entity_encoding' => 'raw',
-            'paste_as_text' => true,
+            'plugins' => [
+                'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
+                'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
+                'insertdatetime', 'media', 'table', 'help', 'wordcount'
+            ],
+            'toolbar' => "undo redo | blocks | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help",
             'language' => 'pt_BR',
-            'content_style' => 'body { font-family: Calibri, Arial, sans-serif; font-size: 11pt; }',
             'branding' => false,
             'promotion' => false,
+            'entity_encoding' => 'raw',
+            'content_style' => 'body { font-family: Calibri, Arial, sans-serif; font-size: 11pt; }'
         ];
 
         $this->clientOptions = ArrayHelper::merge($defaultClientOptions, $this->clientOptions);
