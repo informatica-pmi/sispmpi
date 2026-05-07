20e64cfc7dd1e15d95d6f703f759008c32bd145e
STI - Prefeitura de Itabirito <informatica@pmi.mg.gov.br>
2026-05-07 13:33:51 -0300

    Configurações para ambiente de produção
    Alterações nas configurações para execução no ambiente de Produção.

--- config/db.php
+++ config/db.php
@@ -2,13 +2,13 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=' . getenv('DB_HOST') . ';dbname=' . getenv('DB_NAME'),
+    'dsn' => "mysql:host=" . getenv('DB_HOST') . ";port=" . getenv('DB_PORT') . ";dbname=" . getenv('DB_NAME'),
     'username' => getenv('DB_USER'),
     'password' => getenv('DB_PASS'),
     'charset' => 'utf8',
 
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
+    // Configurações críticas para Produção:
+    'enableSchemaCache' => true,
+    'schemaCacheDuration' => 3600, // 1 hora
+    'schemaCache' => 'cache',
 ];
