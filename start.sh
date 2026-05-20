132abd76f07717d45a5747a0b53815539ccf9a09
STI - Prefeitura de Itabirito <informatica@pmi.mg.gov.br>
2026-05-20 13:07:30 -0300

    Update start.sh
    Injecting key to conatiner on script.

--- start.sh
+++ start.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
+# === NOVO: Força o Apache a repassar as variáveis do Docker para o PHP ===
+echo "PassEnv DB_HOST DB_NAME DB_USER DB_PASSWORD TINYMCE_KEY" > /etc/apache2/conf-available/docker-env.conf
+a2enconf docker-env
+# =========================================================================
+
 # 1. Verifica se a pasta vendor existe. Se não, roda o composer update.
 if [ ! -d "vendor" ]; then
     echo "Instalando dependências do Composer..."
     composer update
 fi
 
-# 2. Aguarda o MySQL estar pronto (simples sleep para garantir que o banco subiu)
+# 2. Aguarda o MySQL estar pronto
 echo "Aguardando o banco de dados..."
 sleep 10
 
-# 3. Roda as migrations automaticamente. 
-# O parâmetro --interactive=0 impede que o terminal peça para você digitar "yes"
+# 3. Roda as migrations automaticamente.
 echo "Rodando migrations do Yii2..."
 php yii migrate --interactive=0
 
