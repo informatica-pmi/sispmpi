39856f3e7e419e9193e99f01e6eadea890aad477
STI - Prefeitura de Itabirito <informatica@pmi.mg.gov.br>
2026-05-21 13:58:48 -0300

    More email Configurations
    Update on email configurations, need to ajust to deprecated library used in original project.

--- web/index.php
+++ web/index.php
@@ -4,6 +4,10 @@
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
+// === NOVO: Ignorar avisos de depreciação do PHP 8.2 ===
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
+// =======================================================
+
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
